2de2987b8d0d3ee2abe9f6397231bdba50fd80a2
mariaanghel21 <153634056+mariaanghel21@users.noreply.github.com>
2024-11-17 21:09:18 +0200

    Update GameStats.h

--- GameStats.h
+++ GameStats.h
@@ -11,6 +11,77 @@ public:
     int getGuessesRemaining() const { return guessesRemaining; }
     bool hasGuessesLeft() const { return guessesRemaining > 0; }
 
+void drawHangman() const {
+        switch (guessesRemaining) {
+            case 6:
+                std::cout << "  _______\n"
+                             " |/      |\n"
+                             " |\n"
+                             " |\n"
+                             " |\n"
+                             " |\n"
+                             "_|___\n";
+                break;
+            case 5:
+                std::cout << "  _______\n"
+                             " |/      |\n"
+                             " |       O\n"
+                             " |\n"
+                             " |\n"
+                             " |\n"
+                             "_|___\n";
+                break;
+            case 4:
+                std::cout << "  _______\n"
+                             " |/      |\n"
+                             " |       O\n"
+                             " |       |\n"
+                             " |\n"
+                             " |\n"
+                             "_|___\n";
+                break;
+            case 3:
+                std::cout << "  _______\n"
+                             " |/      |\n"
+                             " |       O\n"
+                             " |      /|\n"
+                             " |\n"
+                             " |\n"
+                             "_|___\n";
+                break;
+            case 2:
+                std::cout << "  _______\n"
+                             " |/      |\n"
+                             " |       O\n"
+                             " |      /|\\\n"
+                             " |\n"
+                             " |\n"
+                             "_|___\n";
+                break;
+            case 1:
+                std::cout << "  _______\n"
+                             " |/      |\n"
+                             " |       O\n"
+                             " |      /|\\\n"
+                             " |      /\n"
+                             " |\n"
+                             "_|___\n";
+                break;
+            case 0:
+                std::cout << "  _______\n"
+                             " |/      |\n"
+                             " |       O\n"
+                             " |      /|\\\n"
+                             " |      / \\\n"
+                             " |\n"
+                             "_|___\n";
+                break;
+            default:
+                std::cout << "Invalid state\n";
+                break;
+        }
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const GameStats& stats) {
         os << "Guesses Remaining: " << stats.guessesRemaining;
         return os;
