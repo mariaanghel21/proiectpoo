b4db0629d5d6cb2955461c9c09030ad45c7e88aa
mariaanghel21 <153634056+mariaanghel21@users.noreply.github.com>
2024-12-27 16:38:54 +0200

    upt11

--- headers/HangmanGame.h
+++ headers/HangmanGame.h
@@ -7,35 +7,7 @@
 #include "Player.h"
 #include "Word.h"
 #include "GameStats.h"
-#include <stdexcept>  
 
-class HangmanException : public std::exception {
-public:
-    const char* what() const noexcept override {
-        return "General Hangman exception occurred.";
-    }
-};
-
-class InvalidLevelException : public HangmanException {
-public:
-    const char* what() const noexcept override {
-        return "Invalid level selected for Hangman.";
-    }
-};
-
-class WordNotFoundException : public HangmanException {
-public:
-    const char* what() const noexcept override {
-        return "No valid word found for the game.";
-    }
-};
-
-class FileOpenException : public HangmanException {
-public:
-    const char* what() const noexcept override {
-        return "Error opening the word file.";
-    }
-};
 
 class HangmanGame {
 protected:
--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include "headers/MediumModeHangman.h"  
 #include "headers/HardModeHangman.h"
 #include "headers/GameStats.h"
+#include "headers/Exception.h"
 
 void loadWords(std::vector<std::string>& easyWords, std::vector<std::string>& mediumWords, std::vector<std::string>& hardWords) {
     std::ifstream file("cuvinte.txt");
@@ -55,38 +56,37 @@ int main() {
         if (easyWords.empty() || mediumWords.empty() || hardWords.empty()) {
             throw FileOpenException();  
         }
-    }
 
-    std::cout << "----------------------------------\n";
-    std::cout << "       Welcome to Hangman!       \n";
-    std::cout << "----------------------------------\n";
-    std::cout << "Rules of the Game:\n";
-    std::cout << "The player must guess a secret word letter by letter.\n";
-    std::cout << "Each incorrect guess adds a part to the hangman drawing.\n";
-    std::cout << "The player can request a hint by pressing ? (a hint reveals one letter from the word).\n";
-    std::cout << " Requesting a hint deducts points from the total score.\n";
-    std::cout << "The game allows multiple rounds, with the total score carried over between rounds.\n";
-    std::cout << "The player earns points for each word guessed completely.\n";
-    std::cout << "The game ends when the player chooses to stop or loses.\n";
-    std::cout << "Already guessed letters are displayed to avoid repetition.\n";
-    std::cout << "The progress of the guessed word is shown as dashes and letters.\n";
-    std::cout << "The goal is to score as many points as possible and guess all the words.\n";
-    std::cout << "Difficulty levels:\n";
-    std::cout << "   - Easy: Words up to 5 letters, -1 point per hint.\n";
-    std::cout << "   - Medium: Words up to 10 letters, -2 points per hint.\n";
-    std::cout << "   - Hard: Words longer than 10 letters, -3 points per hint.\n";
-    std::cout << "You will be hanged with the blue console cable!\n";
-    std::cout << "Good luck and have fun!\n";
+        std::cout << "----------------------------------\n";
+        std::cout << "       Welcome to Hangman!       \n";
+        std::cout << "----------------------------------\n";
+        std::cout << "Rules of the Game:\n";
+        std::cout << "The player must guess a secret word letter by letter.\n";
+        std::cout << "Each incorrect guess adds a part to the hangman drawing.\n";
+        std::cout << "The player can request a hint by pressing ? (a hint reveals one letter from the word).\n";
+        std::cout << " Requesting a hint deducts points from the total score.\n";
+        std::cout << "The game allows multiple rounds, with the total score carried over between rounds.\n";
+        std::cout << "The player earns points for each word guessed completely.\n";
+        std::cout << "The game ends when the player chooses to stop or loses.\n";
+        std::cout << "Already guessed letters are displayed to avoid repetition.\n";
+        std::cout << "The progress of the guessed word is shown as dashes and letters.\n";
+        std::cout << "The goal is to score as many points as possible and guess all the words.\n";
+        std::cout << "Difficulty levels:\n";
+        std::cout << "   - Easy: Words up to 5 letters, -1 point per hint.\n";
+        std::cout << "   - Medium: Words up to 10 letters, -2 points per hint.\n";
+        std::cout << "   - Hard: Words longer than 10 letters, -3 points per hint.\n";
+        std::cout << "You will be hanged with the blue console cable!\n";
+        std::cout << "Good luck and have fun!\n";
 
-    std::string playerName;
-    std::cout << "Enter player name: ";
-    std::getline(std::cin, playerName);
+        std::string playerName;
+        std::cout << "Enter player name: ";
+        std::getline(std::cin, playerName);
 
-    int level;
-    std::cout << "Select difficulty level (1: Easy, 2: Medium, 3: Hard): ";
-    std::cin >> level;
+        int level;
+        std::cout << "Select difficulty level (1: Easy, 2: Medium, 3: Hard): ";
+        std::cin >> level;
 
-    std::string wordToGuess;
+        std::string wordToGuess;
         if (level == 1) {
             wordToGuess = getRandomWord(easyWords);
         } else if (level == 2) {
@@ -101,62 +101,67 @@ int main() {
             throw WordNotFoundException();  
         }
 
+        Player player(playerName);
+        int totalGames = 0;
+        int totalScore = 0;
+        bool playAgain = true;
 
-    Player player(playerName);
-    int totalGames = 0;
-    int totalScore = 0;
-    bool playAgain = true;
-
-    while (playAgain) {
-        
-        std::unique_ptr<HangmanGame> game;
-
-        if (level == 1) {
-
-            game = std::make_unique<EasyModeHangman>(std::make_unique<Player>(player), std::make_unique<Word>(wordToGuess), 6);
-
-        } else if (level == 2) {
-
-            game = std::make_unique<MediumModeHangman>(std::make_unique<Player>(player), std::make_unique<Word>(wordToGuess), 6);
-
-        } else if (level == 3) {
-
-            game = std::make_unique<HardModeHangman>(std::make_unique<Player>(player), std::make_unique<Word>(wordToGuess), 6);
-
-        }
-
-        game->play(level);
-
-        totalGames++;
-        totalScore += player.getScore();
-
-        std::cout << "Total Games Played: " << totalGames << "\n";
-        std::cout << "Total Score: " << totalScore << "\n";
-
-        char playChoice;
-        std::cout << "Do you want to play again? (y/n): ";
-        std::cin >> playChoice;
-
-        if (playChoice != 'y' && playChoice != 'Y') {
-            playAgain = false;
-        } else {
-
-            std::cout << "Select difficulty level for the next round (1: Easy, 2: Medium, 3: Hard): ";
-            std::cin >> level;
+        while (playAgain) {
+            std::unique_ptr<HangmanGame> game;
 
             if (level == 1) {
-                wordToGuess = getRandomWord(easyWords);
+
+                game = std::make_unique<EasyModeHangman>(std::make_unique<Player>(player), std::make_unique<Word>(wordToGuess), 6);
+
             } else if (level == 2) {
-                wordToGuess = getRandomWord(mediumWords);
+                game = std::make_unique<MediumModeHangman>(std::make_unique<Player>(player), std::make_unique<Word>(wordToGuess), 6);
+
             } else if (level == 3) {
-                wordToGuess = getRandomWord(hardWords);
+
+                game = std::make_unique<HardModeHangman>(std::make_unique<Player>(player), std::make_unique<Word>(wordToGuess), 6);
+
+            }
+
+            game->play(level);
+
+            totalGames++;
+            totalScore += player.getScore();
+
+            std::cout << "Total Games Played: " << totalGames << "\n";
+            std::cout << "Total Score: " << totalScore << "\n";
+
+            char playChoice;
+            std::cout << "Do you want to play again? (y/n): ";
+            std::cin >> playChoice;
+
+            if (playChoice != 'y' && playChoice != 'Y') {
+                playAgain = false;
+
+            } else {
+                
+                std::cout << "Select difficulty level for the next round (1: Easy, 2: Medium, 3: Hard): ";
+                std::cin >> level;
+
+                if (level == 1) {
+                    wordToGuess = getRandomWord(easyWords);
+                } else if (level == 2) {
+                    wordToGuess = getRandomWord(mediumWords);
+                } else if (level == 3) {
+                    wordToGuess = getRandomWord(hardWords);
+                }
             }
         }
 
+        std::cout << "\nTotal Games Played: " << totalGames << "\n";
+        std::cout << "Total Score: " << totalScore << "\n";
+
+        return 0;
+
+    } catch (const FileOpenException& e) {
+        std::cerr << e.what() << "\n";
+    } catch (const InvalidLevelException& e) {
+        std::cerr << e.what() << "\n";
+    } catch (const WordNotFoundException& e) {
+        std::cerr << e.what() << "\n";
     }
-
-    std::cout << "\nTotal Games Played: " << totalGames << "\n";
-    std::cout << "Total Score: " << totalScore << "\n";
-
-    return 0;
 }
